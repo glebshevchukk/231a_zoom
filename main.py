083970b923203fb7b5c27ea14464cf2b2da07b55
glebshevchukk <glebs@stanford.edu>
2021-03-09 12:01:15 -0800

    Fixed bugs and got a few diff examples

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ from PIL import Image, ImageDraw
 
 #assuming for now that images are taken in reverse-z (closer to farther strictly)
 
-def load_images(image_dir,num_images):
+def load_images(image_dir,num_images,has_depth,extension=".jpg"):
     images, depth_images = [], []
     for i in range(num_images):
-        images.append(image_dir+str(i)+".jpg")
-        depth_images.append(image_dir+str(i)+"_depth.jpg")
+        images.append(image_dir+str(i)+extension)
+        if has_depth:
+            depth_images.append(image_dir+str(i)+"_depth"+extension)
     return images,depth_images
 
-def linear_interpolate(images,interpolate_rate=10):
+def linear_interpolate(images,interpolate_rate=5):
     '''
     Create a video interpolating between the frames by taking a linear
     weight of each pair of neighbor frames
@@ -22,18 +23,19 @@ def linear_interpolate(images,interpolate_rate=10):
     iplated = []
     prev = images[0]
     for frame in images[1:]:
-        for i in range(1,interpolate_rate):
+        for i in range(1,interpolate_rate+1):
             weight = float(i) / interpolate_rate
-            mid_frame = cv2.addWeighted(prev,weight,frame,1-weight,0) 
+            mid_frame = cv2.addWeighted(prev,1-weight,frame,weight,0) 
             iplated.append(Image.fromarray(mid_frame))
         prev = frame
     return iplated
 
-def flow_interpolate(images,interpolate_rate=20):
+def flow_interpolate(images,interpolate_rate=5):
     # Calculates dense optical flow by Farneback method 
     prev = images[0]
     iplated = []
     for frame in images[1:]:
+        
         prev_gray = cv2.cvtColor(prev, cv2.COLOR_RGB2GRAY) 
         gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY) 
         flow = cv2.calcOpticalFlowFarneback(prev_gray,gray, None, 0.5, 3, 25, 3, 7, 1.2, cv2.OPTFLOW_FARNEBACK_GAUSSIAN) 
@@ -44,13 +46,12 @@ def flow_interpolate(images,interpolate_rate=20):
         flow[:,:,1] += np.arange(h)[:,np.newaxis]
         mid_frame = cv2.remap(prev,flow,None,cv2.INTER_LINEAR)
 
-        for i in range(1,interpolate_rate):
+        for i in range(1,interpolate_rate+1):
            weight = float(i) / interpolate_rate
-           new_frame = cv2.addWeighted(prev,weight,mid_frame,1-weight,0) 
+           new_frame = cv2.addWeighted(prev,1-weight,mid_frame,weight,0) 
            iplated.append(Image.fromarray(new_frame))
-
-        #iplated.append(Image.fromarray(frame))
         prev = frame
+    print(len(iplated))
     return iplated
 
 
@@ -58,32 +59,36 @@ def flow_interpolate(images,interpolate_rate=20):
 def run_dolly(config):
     save_path = config.image_dir + config.save_name
     #PREPROCESSING
-    final_images = []
+
     li, lid,segd = [],[],[]
-    images,depth_images = load_images(config.image_dir,config.num_images)
+    images,depth_images = load_images(config.image_dir,config.num_images, config.has_depth,config.extension)
     #load everything in and threshold what's needed
     for image in images:
         l = cv2.imread(image)
         l=cv2.cvtColor(l, cv2.COLOR_BGR2RGB)
-        l = cv2.resize(l ,(config.height,config.width)) 
+        l = cv2.resize(l ,(config.width,config.height)) 
         li.append(l)
-    for i,depth in enumerate(depth_images):
-        d = cv2.imread(depth)
-        r,d = cv2.threshold(d,config.threshold,1,cv2.THRESH_BINARY_INV)
-        d = cv2.resize(d ,(config.height,config.width)) 
-        lid.append(d)
-        seg = li[i]*d
-        segd.append(seg)
-    
-    final_images.append(li[0])
-
+    if config.has_depth:
+        for i,depth in enumerate(depth_images):
+            d = cv2.imread(depth)
+            r,d = cv2.threshold(d,config.threshold,1,cv2.THRESH_BINARY_INV)
+            d = cv2.resize(d ,(config.width,config.height)) 
+            lid.append(d)
+            seg = li[i]*d
+            segd.append(seg)
     #get orb features and match them
     #adapting from this cv2 tutorial: https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_feature2d/py_matcher/py_matcher.html#flann-based-matcher
     #and https://docs.opencv.org/3.4/d5/d6f/tutorial_feature_flann_matcher.html
-    for i,rest_seg in enumerate(segd[1:]):   
+
+    final_images = [li[0]]
+    if config.has_depth:
+        use_for_features = segd
+    else:
+        use_for_features = li
+    for i in range(len(use_for_features)-1):   
         orb = cv2.ORB_create()
-        kp1,des1 = orb.detectAndCompute(segd[0],None)
-        kp2,des2 = orb.detectAndCompute(segd[i],None)
+        kp1,des1 = orb.detectAndCompute(use_for_features[0],None)
+        kp2,des2 = orb.detectAndCompute(use_for_features[i+1],None)
         
         bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
         matches = bf.match(des1, des2)
@@ -93,8 +98,7 @@ def run_dolly(config):
         src_points = np.float32([kp1[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
         dst_points = np.float32([kp2[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
         X,_ = cv2.estimateAffinePartial2D(dst_points,src_points)
-        final_img = cv2.warpAffine(li[i], X,(config.height,config.width))
-        d = cv2.resize(d ,(config.height,config.width)) 
+        final_img = cv2.warpAffine(li[i+1], X,(config.width,config.height))
         final_images.append(final_img)
 
     #interpolate between all images smoothly
@@ -114,10 +118,12 @@ if __name__ == "__main__":
     parser.add_argument('--interpolation_type',type=str,default='flow')
     parser.add_argument('--image_dir',type=str,default="img/maggie/")
     parser.add_argument('--save_name',type=str,default="dollied.gif")
-    parser.add_argument('--num_images',type=int,default=3)
-    parser.add_argument('--width',type=int,default=1008)
-    parser.add_argument('--height',type=int,default=756)
+    parser.add_argument('--extension',type=str,default=".jpg")
+    parser.add_argument('--num_images',type=int,default=4)
+    parser.add_argument('--height',type=int,default=1008)
+    parser.add_argument('--width',type=int,default=756)
     parser.add_argument('--threshold',type=int,default=220)
     parser.add_argument('--n_best_matches',type=int,default=30)
+    parser.add_argument('--has_depth',type=bool,default=False)
     config = parser.parse_args()
     run_dolly(config)
